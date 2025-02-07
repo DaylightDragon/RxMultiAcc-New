2fa7cbf3a100f2880ffac4ca70bcbef9dfe04489
DaylightDragon <work.daylight@gmail.com>
2025-02-08 00:58:03 +0300

    Renaming things

--- RxMultiAcc/Properties/AssemblyInfo.cs
+++ RxMultiAcc/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("RxMultiAcc")]
+[assembly: AssemblyTitle("RbxMultiAcc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RxMultiAcc")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyProduct("RbxMultiAcc")]
+[assembly: AssemblyCopyright("Copyright © DaylightDragon 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
